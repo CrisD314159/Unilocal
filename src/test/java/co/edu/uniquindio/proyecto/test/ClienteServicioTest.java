106fc7c64025a90dd51ffca739931f4490c0cf3d
Cristian David <cristiandavidvargas2@icloud.com>
2024-05-23 22:04:56 -0500

    Adicion base de datos atlas

--- src/test/java/co/edu/uniquindio/proyecto/test/ClienteServicioTest.java
+++ src/test/java/co/edu/uniquindio/proyecto/test/ClienteServicioTest.java
@@ -23,57 +23,8 @@ public class ClienteServicioTest {
     private UsuarioServicioImp usuarioServicio;
 
 
-    @Test
-    public void registrarseTest() throws Exception {
-        File file = new File("src/test/resources/Javascript.png");
-        InputStream inputStream = new FileInputStream(file);
-        MockMultipartFile multipartFile = new MockMultipartFile("imagen", file.getName(), "image/jpeg", inputStream);
-        RegistroClienteDTO registroClienteDTO = new RegistroClienteDTO(
-                "Cristian David Vargas",
-
-                multipartFile,
-
-                "crisvargas1234",
-
-                "davidclaff4@gmail.com",
-
-               "cris123",
-
-                "Medellin, Colombia"
-        );
-
-        boolean respuesta = usuarioServicio.registrase(registroClienteDTO);
-        Assertions.assertTrue(respuesta);
-    }
-
-    @Test
-    public void editarperfilTest() throws IOException {
-        File file = new File("src/test/resources/Javascript.png");
-        InputStream inputStream = new FileInputStream(file);
-        MockMultipartFile multipartFile = new MockMultipartFile("imagen", file.getName(), "image/jpeg", inputStream);
-        ActualizarUsuarioDTO actualizarUsuarioDTO = new ActualizarUsuarioDTO(
-                "660976c557c6105686a33bc9",
-
-                "David Vargas",
-
-                multipartFile,
-
-                "cris123",
-
-                "cris123",
-
-                "Armenia, Colombia"
-        );
-        boolean respuesta = false;
-        try {
-             respuesta = usuarioServicio.editarPerfil(actualizarUsuarioDTO);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        Assertions.assertTrue(respuesta);
 
 
-    }
 
     @Test
     public void encontrarUsuarioTest(){
--- src/test/java/co/edu/uniquindio/proyecto/test/LugarServicioTest.java
+++ src/test/java/co/edu/uniquindio/proyecto/test/LugarServicioTest.java
@@ -29,75 +29,9 @@ public class LugarServicioTest {
     @Autowired
     private LugarServicioImp lugarServicioImp;
 
-    @Test
-    public void crearLugarTest() throws IOException {
-        ArrayList<String> telefonos = new ArrayList<>();
-        ArrayList<MultipartFile> imagenes = new ArrayList<>();
-        ArrayList<Horario> horarios = new ArrayList<>();
-        File file = new File("src/test/resources/Javascript.png");
-        InputStream inputStream = new FileInputStream(file);
-        MockMultipartFile multipartFile = new MockMultipartFile("imagen", file.getName(), "image/jpeg", inputStream);
-
-        horarios.add(new Horario("Lunes", "7:00am", "9:00pm"));
-        telefonos.add("3122341232");
-        imagenes.add(multipartFile);
-        CrearNegocioDTO crearNegocioDTO= new CrearNegocioDTO(
-                "Este es un hotel ubicado en el centro de la ciudad",
-                "HostLeeping",
-                telefonos,
-                imagenes,
-                Categoria.HOTEL,
-                new Coordenada("123.1", "49944.2"),
-                "660976c557c6105686a33bc9",
-                horarios
 
 
-        );
-        boolean respuesta = false;
-        try {
-            respuesta = lugarServicioImp.crearLugar(crearNegocioDTO);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        Assertions.assertTrue(respuesta);
 
-
-    }
-
-    @Test
-    public void actualizarnegocioTest() throws IOException {
-        ArrayList<String> telefonos = new ArrayList<>();
-        ArrayList<MultipartFile> imagenes = new ArrayList<>();
-        ArrayList<Horario> horarios = new ArrayList<>();
-        File file = new File("src/test/resources/python.png");
-        InputStream inputStream = new FileInputStream(file);
-        MockMultipartFile multipartFile = new MockMultipartFile("imagen", file.getName(), "image/jpeg", inputStream);
-
-        horarios.add(new Horario("Lunes", "7:00am", "9:00pm"));
-        telefonos.add("3122341232");
-        imagenes.add(multipartFile);
-        ActualizarNegocioDTO actualizarNegocioDTO= new ActualizarNegocioDTO(
-                "66098099c213596ba18c73c3",
-                "Este es un negocio de comida rápida ubicado en el centro de la ciudad",
-                "FastFoood",
-                telefonos,
-                imagenes,
-                new Coordenada("92.1", "31.2"),
-                horarios
-
-
-        );
-
-        boolean respuesta;
-        try {
-            respuesta =  lugarServicioImp.actualizarNegocio(actualizarNegocioDTO);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        Assertions.assertTrue(respuesta);
-
-
-    }
 
     @Test
     public void eliminarLugarTest(){
